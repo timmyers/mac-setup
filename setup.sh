ae45101e5d1b94140033af969973b5e1f76afb45
Tim <tim@Megans-MacBook.local>
2019-03-05 18:09:39 -0700

    Install google-cloud-sdk

--- setup.sh
+++ setup.sh
@@ -86,3 +86,6 @@ brew install pyenv
 echo "" >> ~/.zshrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 source ~/.zshrc
+
+# Install google-cloud==-sdk
+brew cask intall google-cloud-sdk
