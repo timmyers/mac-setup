27c94b99067ca4868e186f883b0d57b612ec47df
Tim <tim@Megans-MacBook.local>
2019-03-05 18:01:12 -0700

    More pyenv setup

--- setup.sh
+++ setup.sh
@@ -83,3 +83,6 @@ brew install gcloud
 
 # Install pyenv
 brew install pyenv
+echo "" >> ~/.zshrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+source ~/.zshrc
